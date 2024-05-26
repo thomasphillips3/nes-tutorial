631c6c7afb15271e2117ab0a66c59f3448d97ace
Thomas Phillips <thomasphillips3@gmail.com>
2024-05-26 10:53:08 -0700

    load background into memory

--- nes-tutorial.asm
+++ nes-tutorial.asm
@@ -8,10 +8,45 @@
   .inesmap 0    ; Defines the NES mapper
   .inesmir 1    ; Defines VRAM mirroring of banks
 
+  .rsset $0000
+  pointerBackgroundLowByte  .rs 1
+  pointerBackgroundHighByte .rs 1
+
   .bank 0
   .org $C000
 
 RESET:
+  JSR LoadBackground
+
+LoadBackground: ; Load the value #$2000 to the address $2006
+  LDA $2002 ; Load $2002 to the Accumulator to reset the PPU 
+  LDA #$20  ; Load the first byte (#$20) to the Accumulator
+  STA $2006 ; Store the value from the Accumulator (#$20) to address $2006
+  LDA #$00  ; Load the second byte (#$00) to the Accumulator
+  STA $2006 ; Store the value from the Accumulator (#$00) to address $2006
+
+; Load the background into the memory
+  LDA #LOW(background)
+  STA pointerBackgroundLowByte
+  LDA #HIGH(background)
+  STA pointerBackgroundHighByte
+
+; Initialize x and y registers to #$00
+  LDX #$00
+  LDY #$00
+.Loop:
+  LDA [pointerBackgroundLowByte], y
+  STA $2007
+
+  INY
+  CPY #$00
+  BNE .Loop
+
+  INC pointerBackgroundHighByte
+  INX
+  CPX #$04
+  BNE .Loop
+  RTS
 
 InfiniteLoop:
   JMP InfiniteLoop
